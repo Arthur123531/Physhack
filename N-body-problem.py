1e81747e1ad3cc6deb8caef568c449a844d9f8e6
Arthur <arthur.descrivan@gmail.com>
2024-11-15 18:51:12 -0500

    Fixing my sloppy code

--- N-body-problem.py
+++ N-body-problem.py
@@ -9,8 +9,8 @@ class CelestialBody:
     def __init__(self, name, mass, position, velocity):
         self.name = name
         self.mass = mass
-        self.position = np.array(position)
-        self.velocity = np.array(velocity)
+        self.position = np.array(position).astype(float)
+        self.velocity = np.array(velocity).astype(float)
 
 def calculate_force(body1, body2):
     r_vec = body2.position - body1.position
@@ -34,13 +34,21 @@ def update_bodies(bodies, dt):
 
 # Simulation parameters
 dt = 3600  # Time step in seconds (1 hour)
-total_time = 31536000  # Total simulation time in seconds (1 year)
+total_time = 31536000 * 10  # Total simulation time in seconds (1 year)
 
 # Create celestial bodies
 sun = CelestialBody("Sun", 1.989e30, [0, 0, 0], [0, 0, 0])
+mercury = CelestialBody("Mercury", 3.285e23, [57.9e9, 0, 0], [0, 47.87e3, 0])
+venus = CelestialBody("Venus", 4.867e24, [108.2e9, 0, 0], [0, 35.02e3, 0])
 earth = CelestialBody("Earth", 5.972e24, [149.6e9, 0, 0], [0, 29.78e3, 0])
+mars = CelestialBody("Mars", 6.390e23, [227.9e9, 0, 0], [0, 24.07e3, 0])
+jupiter = CelestialBody("Jupiter", 1.898e27, [778.5e9, 0, 0], [0, 13.07e3, 0])
+saturn = CelestialBody("Saturn", 5.683e26, [1433.5e9, 0, 0], [0, 9.69e3, 0])
+uranus = CelestialBody("Uranus", 8.681e25, [2872.5e9, 0, 0], [0, 6.81e3, 0])
+neptune = CelestialBody("Neptune", 1.024e26, [4495.1e9, 0, 0], [0, 5.43e3, 0])
+#moon = CelestialBody("Moon", 7.342e22, [149.6e9 + 384.4e6, 0, 0], [0, 29.78e3 + 1.022e3, 0])
 
-bodies = [sun, earth]
+bodies = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]  # List of celestial bodies
 
 # Run simulation
 positions = np.zeros((int(total_time/dt), len(bodies), 3))
