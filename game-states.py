fbb0fcb9f88a74b24def4356a3c6fecb302753e4
Victor <levictorswim@gmail.com>
2024-11-16 15:15:30 -0500

    removed main from N-body, merged into game-states, added main-copy as backup

--- game-states.py
+++ game-states.py
@@ -1,115 +1,6 @@
-import pygame
-import math
-import numpy as np
-from  random import randint
+from N_body_problem import *
 
-
-
-# Window dimensions
-WIDTH, HEIGHT = 1040, 680
-WIDTH, HEIGHT = 1040, 680
-
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-ORANGE = (255, 165, 0)
-PINK = (255, 192, 203)
-PURPLE = (128, 0, 128)
-YELLOW = (255, 255, 0)
-
-#Constants
-M_EARTH = 5.972e24
-M_ASTEROID = 1e12
-R_EARTH = 6371e3
-Scaling = R_EARTH/25
-G = 6.67430e-11
-speed_cap = 2
-
-
-
-
-class Body:
-    def __init__(self, x:int|None = None, y:int|None = None, vx:float|None = None, vy:float|None = None, mass:float|None = None, radius:int|None = None, color:str|tuple[int]|None = None, body_type:str = "asteroid", type_props = {}):
-        if x is not None:
-            self.x = x
-        elif body_type == "asteroid":
-            self.x = randint(WIDTH//4, 3*WIDTH//4)
-        else:
-            self.x = WIDTH//2
-        if y is not None:
-            self.y = y
-        elif body_type == "asteroid":
-            self.y = randint(HEIGHT //4, 3*HEIGHT//4)
-        else:
-            self.y = HEIGHT//2
-        if vx is not None:
-            self.vx = vx
-        else:
-            self.vx = 0
-        if vy is not None:
-            self.vy = vy
-        else:
-            self.vy = 0
-        if mass is not None:
-            self.mass = mass
-        elif body_type == "asteroid":
-            self.mass = M_ASTEROID
-        elif body_type == "planet":
-            self.mass = M_EARTH
-        if radius is not None:
-            self.radius = radius
-        elif body_type == "planet":
-            self.radius = 25
-        else:
-            self.radius = 5
-        if color is not None:
-            self.color = color
-        elif body_type == "planet":
-            self.color = BLUE
-        else:
-            self.color = BLACK
-        self.trail = [(self.x, self.y)]
-        self.body_type = body_type
-        #if self.body_type == "asteroid":
-            #self.visible = type_props["visible"]
-
-    def update_position(self):
-        self.x += self.vx
-        self.x -= EARTH.vx
-        self.y += self.vy
-        self.trail.append((self.x, self.y))
-
-    def apply_force(self, fx, fy):
-        ax = fx / self.mass
-        ay = fy / self.mass
-        if (ax**2+ay**2)**.5 < 10:
-            self.vx += ax
-            self.vy += ay
-        if self.vx**2+self.vy**2 > speed_cap**2:
-            self.vx /= (self.vx**2+self.vy**2)**.5
-            self.vy /= (self.vx**2+self.vy**2)**.5
-            self.vx *= speed_cap
-            self.vy *= speed_cap
-
-def calculate_force(body1, body2):
-    dx = (body2.x - body1.x)*Scaling
-    dy = (body2.y - body1.y)*Scaling
-    dist = math.sqrt(dx*dx + dy*dy)
-    if dist > 0:
-        force = G * body1.mass * body2.mass / (dist * dist)
-        angle = math.atan2(dy, dx)
-        fx = force * math.cos(angle)
-        fy = force * math.sin(angle)
-        return fx, fy
-    else:
-        return 0, 0
-
-#Planet
-EARTH = Body(body_type="planet")
-
+TIME_LIMIT = 1000
 
 class State(object):
     def __init__(self):
@@ -124,21 +15,39 @@ class Start(State):
         self.next = 'game'
 
     def cleanup(self):
-        print('Cleaning Start state')
+        print('')
 
     def startup(self):
-        self.state_text = state_font.render('Start', True, (0, 0, 0))
+        self.menu_text = get_font(50).render('PLANET LAND', True, '#b68f40')
+        self.menu_rect = self.menu_text.get_rect(center=(WIDTH // 2, 100))
+        self.menu_mouse_pos = pygame.mouse.get_pos()
 
     def get_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            self.done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.play_button.checkForInput(self.menu_mouse_pos):
+                self.done = True
+            if self.quit_button.checkForInput(self.menu_mouse_pos):
+                self.quit = True
+
 
     def update(self, screen, dt):
+        self.menu_mouse_pos = pygame.mouse.get_pos()
+
+        self.play_button = Button(image=pygame.image.load('Play Rect.png'), pos=(WIDTH // 2, 250),
+                             text_input='PLAY', font=get_font(75), base_color='#d7fcd4', hovering_color='White')
+        self.quit_button = Button(image=pygame.image.load('Quit Rect.png'), pos=(WIDTH // 2, 450),
+                             text_input='QUIT', font=get_font(75), base_color='#d7fcd4', hovering_color='White')
+
+        for button in [self.play_button, self.quit_button]:
+            button.changeColor(self.menu_mouse_pos)
+
         self.draw(screen)
 
     def draw(self, screen):
-        screen.fill((255, 255, 255))
-        screen.blit(self.state_text, (0, 0))
+        screen.blit(BG, (0, 0))
+        screen.blit(self.menu_text, self.menu_rect)
+        for button in [self.play_button, self.quit_button]:
+            button.update(screen)
 
 class Game(State):
     def __init__(self):
@@ -146,24 +55,26 @@ class Game(State):
         self.next = 'win'
 
     def cleanup(self):
-        print('Cleaning Game state')
+        pygame.time.set_timer(pygame.USEREVENT + 1, 1000)
+        self.timer_counter = TIME_LIMIT
+        EARTH.counter = 0
 
     def startup(self):
         # self.state_text = state_font.render('Gaming', True, (255, 255, 255))
 
         self.timer_event = pygame.USEREVENT + 1
         pygame.time.set_timer(self.timer_event, 1000)
-        self.timer_counter = 10  # Change to whatever time we give
+        self.timer_counter = TIME_LIMIT  # Change to whatever time we give
 
         self.timer_font = pygame.font.SysFont('Arial', 30)
         self.timer_text = self.timer_font.render('Timer: ' + str(self.timer_counter), True, BLACK)
 
         self.bodies = [EARTH]
-        for i in range(5):
-            self.bodies.append(Body(body_type="asteroid"))
 
     def get_event(self, event):
         if event.type == self.timer_event:
+            if self.timer_counter % 10 == 0:
+                self.bodies.append(Body(body_type="asteroid"))
             self.timer_counter -= 1
             self.timer_text = self.timer_font.render('Timer: ' + str(self.timer_counter), True, BLACK)
             if self.timer_counter == 0:
@@ -174,6 +85,7 @@ class Game(State):
     def update(self, screen, dt):
 
         for i, body in enumerate(self.bodies):
+            collisions = 0
             for j in range(len(self.bodies)):
                 if i != j:
                     fx, fy = calculate_force(body, self.bodies[j])
@@ -181,16 +93,29 @@ class Game(State):
 
             body.update_position()
 
+            # Ensure bodies stay within the screen boundaries
+            if body.x < 0 or body.x > WIDTH:
+                body.vx *= -1
+            if body.y < 0 or body.y > HEIGHT:
+                body.vy *= -1
+        if body != EARTH:
+            if body.circle.colliderect(EARTH.circle):
+                self.bodies.remove(body)
+                EARTH.counter += 1
+                if len(self.bodies) == 1:
+                    self.bodies.append(Body(body_type="asteroid"))
+            if EARTH.counter > 1:
+                self.next = 'game_over'
+                self.done = True
+
         self.draw(screen)
 
     def draw(self, screen):
         screen.fill((255, 255, 255))
 
-        for i, body in enumerate(self.bodies):
-            for k in range(max(0, len(body.trail) - 500), len(body.trail)):
-                pygame.draw.circle(screen, body.color, (int(body.trail[k][0]), int(body.trail[k][1])), 2)
-
-            pygame.draw.circle(screen, body.color, (int(body.x), int(body.y)), body.radius)
+        for body in self.bodies:
+            body.draw_body()
+            body.tail_display()
 
         screen.blit(self.timer_text, (0, 0))
 
@@ -217,12 +142,52 @@ class Win(State):
         screen.fill((255, 255, 255))
         screen.blit(self.state_text, (0, 0))
 
+class Game_Over(State):
+    def __init__(self):
+        super().__init__()
+        self.next = 'game'
+
+    def cleanup(self):
+        print('Cleaning game over state')
+
+    def startup(self):
+        self.menu_text = get_font(50).render('GAME OVER', True, '#b68f40')
+        self.menu_rect = self.menu_text.get_rect(center=(WIDTH // 2, 100))
+        self.menu_mouse_pos = pygame.mouse.get_pos()
+
+    def get_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.play_button.checkForInput(self.menu_mouse_pos):
+                self.done = True
+            if self.quit_button.checkForInput(self.menu_mouse_pos):
+                self.quit = True
+
+
+    def update(self, screen, dt):
+        self.menu_mouse_pos = pygame.mouse.get_pos()
+
+        self.play_button = Button(image=pygame.image.load('Play Rect.png'), pos=(WIDTH // 2, 250),
+                             text_input='PLAY AGAIN', font=get_font(75), base_color='#d7fcd4', hovering_color='White')
+        self.quit_button = Button(image=pygame.image.load('Quit Rect.png'), pos=(WIDTH // 2, 450),
+                             text_input='QUIT', font=get_font(75), base_color='#d7fcd4', hovering_color='White')
+
+        for button in [self.play_button, self.quit_button]:
+            button.changeColor(self.menu_mouse_pos)
+
+        self.draw(screen)
+
+    def draw(self, screen):
+        screen.blit(BG, (0, 0))
+        screen.blit(self.menu_text, self.menu_rect)
+        for button in [self.play_button, self.quit_button]:
+            button.update(screen)
+
 # Object that runs the game loop and control the switching of states
 class Control:
     def __init__(self, **settings):
         self.__dict__.update(settings)
         self.done = False
-        self.screen = pygame.display.set_mode(self.size)
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
 
     # Setup the initial state before running main game loop
@@ -259,26 +224,22 @@ class Control:
     # main game loop
     def main_game_loop(self):
         while not self.done:
-            delta_time = self.clock.tick(self.fps) / 1000.0
+            delta_time = self.clock.tick(60) / 1000.0
             self.event_loop()
             self.update(delta_time)
             pygame.display.update()
-            self.clock.tick(60)
+
 
 pygame.init()
 
 state_font = pygame.font.SysFont("Arial", 32)
 
-settings = {
-    'size': (1040, 680),
-    'fps': 60
-}
-
-app = Control(**settings)
+app = Control()
 state_dict = {
     'start': Start(),
     'game': Game(),
-    'win': Win()
+    'win': Win(),
+    'game_over': Game_Over()
 }
 app.setup_states(state_dict, 'start')
 app.main_game_loop()
